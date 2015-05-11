3ea6fb4231055491d5db4fc7aef58a38393760d8
Richard Rodger <richard@ricebridge.com>
2015-05-11 13:41:16 +0100

    docker beanstalk

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # salestax-basic
 salestax-basic
+
+
+# v2 docker
+
+```sh
+docker build -t salestax-basic-v2 .
+docker run -p 51001:51001 -e WEB_HOST=0.0.0.0 -e BS_HOST=192.168.59.103 -d salestax-basic-v2 node /service/salestax-basic-main.js
+```
---
+++ service/salestax-basic-main.js
@@ -0,0 +1,19 @@
+var WEB_PORT = process.env.WEB_PORT || 51003
+var WEB_HOST = process.env.WEB_HOST || 'localhost'
+var BS_PORT  = process.env.BS_PORT  || 11300
+var BS_HOST  = process.env.BS_HOST  || 'localhost'
+
+require('seneca')()
+  .use('..', {rate:0.20} )
+
+  .use('beanstalk-transport')
+  .listen({
+    type: 'beanstalk',
+    pin:  'role:salestax,cmd:calculate',
+    port:  BS_PORT,
+    host:  BS_HOST  
+  })
+  .listen({
+    port:WEB_PORT, 
+    host:WEB_HOST
+  })
