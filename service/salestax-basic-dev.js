0831765ca5f58d6872296e99fed91f1ea1af5362
Richard Rodger <richard@ricebridge.com>
2015-05-11 17:45:49 +0100

    host

--- service/salestax-basic-dev.js
+++ service/salestax-basic-dev.js
@@ -1,5 +1,6 @@
 var PORT = process.env.PORT || 51001
+var HOST = process.env.HOST || 'localhost'
 
 require('seneca')()
   .use('..', {rate:0.20} )
-  .listen( PORT )
+  .listen( {port:PORT,host:HOST} )
